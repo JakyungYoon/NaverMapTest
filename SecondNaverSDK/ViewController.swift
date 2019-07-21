d3a90d5b3854240726c5e97a7ac9a3a07a7d7dcb
JakyungYoon <jakyung8@gmail.com>
2019-07-21 16:18:09 +0900

    Initial Commit

---
+++ SecondNaverSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondNaverSDK
+//
+//  Created by Jakyung Yoon on 7/21/19.
+//  Copyright © 2019 Jakyung Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SecondNaverSDKTests/SecondNaverSDKTests.swift
@@ -0,0 +1,34 @@
+//
+//  SecondNaverSDKTests.swift
+//  SecondNaverSDKTests
+//
+//  Created by Jakyung Yoon on 7/21/19.
+//  Copyright © 2019 Jakyung Yoon. All rights reserved.
+//
+
+import XCTest
+@testable import SecondNaverSDK
+
+class SecondNaverSDKTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
